e026e271fc813b4fbcf299fc37dd44779c86a6fa
Dariel800XD <darielodalvis51@gmail.com>
2024-06-24 02:04:09 -0400

    probando3

---
+++ Practica02.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.practica02;
+
+import java.util.Scanner;
+
+public class Practica02 {
+
+    public static void main(String[] args) {
+        
+        double temperatura;
+        int escala;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Bienvenido a un conversor, porfavor ingrese la temperatura:");
+        temperatura = input.nextDouble();
+        
+        System.out.println("Ahora ingrese la escala");
+        System.out.println("Celcius(1) o Farenhair(2)");
+        
+        escala = input.nextInt();
+        
+        if(escala == 1){
+            double celcius =(temperatura - 32) * 5/9;
+            System.out.println(celcius);
+            
+        }
+        else {
+            if(escala == 2){
+                double farenhair = temperatura * 1.8 + 32;
+                System.out.println(farenhair);
+            }
+            else {
+                System.out.println("Porfavor ponga uno de los valores que se le mostro, Gracias");
+            }
+        }
+         
+        
+    }
+}
